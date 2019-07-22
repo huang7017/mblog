bc720d6ed874de39ad2cbb9f23c2c1d62739e151
huang7017 <huang7017@gmail.com>
2019-07-21 23:56:47 -0700

    20190722

--- mainsite/views.py
+++ mainsite/views.py
@@ -8,10 +8,10 @@ def homepage(request):
     posts = Post.objects.all()
     now =datetime.now()
     return render(request,'index.html',locals())
-    
+
 def showpost(request,slug):
     try:
-        post = Post.objects.get(slug =slug)
+        post = Post.objects.get(slug = slug)
         if post != None:
             return render(request,'post.html',locals())
     except:

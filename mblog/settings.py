387914b0d572976140838b4e9ef03b3ed0387e6d
huang7017 <huang7017@gmail.com>
2019-07-23 09:11:51 -0700

    static_url

--- mblog/settings.py
+++ mblog/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '../mainsite/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'../mainsite/static/'),
+    os.path.join(BASE_DIR,'static'),
 ]
 
 db_from_env = dj_database_url.config()

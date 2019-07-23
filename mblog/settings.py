212419470a57661bcee48890ae08131ba6815c97
huang7017 <huang7017@gmail.com>
2019-07-23 06:55:47 -0700

    fix database setting

--- mblog/settings.py
+++ mblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_all
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,5 +123,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+
+db_from_env = dj_database_url.config()
+DATABASE['default'].update(db_from_env)
 
 

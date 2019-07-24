d904eb99213fb4fe082156f0e3e31d282e1e526c
huang7017 <huang7017@gmail.com>
2019-07-24 02:51:23 -0700

    static_url

--- mblog/settings.py
+++ mblog/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../mblog/static'),
+    os.path.join(BASE_DIR, '../mainsite/static'),
 )
 
 db_from_env = dj_database_url.config()

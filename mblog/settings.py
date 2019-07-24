b714d8d01e191025a18ecb405c93c1fd7f2f1619
huang7017 <huang7017@gmail.com>
2019-07-24 02:26:18 -0700

    static_url

--- mblog/settings.py
+++ mblog/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR,'../mblog/static'),
 ]
 
 db_from_env = dj_database_url.config()

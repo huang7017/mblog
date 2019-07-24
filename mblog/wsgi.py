744f9008fdd005b921d5bb92f97b21413d09bf65
huang7017 <huang7017@gmail.com>
2019-07-24 01:54:05 -0700

    static_url

--- mblog/wsgi.py
+++ mblog/wsgi.py
@@ -11,8 +11,7 @@ import os
 
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mblog.settings')
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = Cling(get_wsgi_application())

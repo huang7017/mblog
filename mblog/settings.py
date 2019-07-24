aa69395050373ab0b7508f3f1960d60bd862ed39
huang7017 <huang7017@gmail.com>
2019-07-24 05:29:08 -0700

    static_url

--- mblog/settings.py
+++ mblog/settings.py
@@ -118,12 +118,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../mainsite/static'),
+    os.path.join(BASE_DIR, "static"),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+
+
+
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)

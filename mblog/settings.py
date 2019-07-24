3792008e89f1d0a4275e4fd9ced1fedb4638a406
huang7017 <huang7017@gmail.com>
2019-07-24 01:44:22 -0700

    static_url

--- mblog/settings.py
+++ mblog/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'mblog.urls'
 
 TEMPLATES = [

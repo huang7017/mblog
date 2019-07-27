f348c74c62ecaa1b60912f2294a377dca9269c2d
huang7017 <huang7017@gmail.com>
2019-07-27 01:56:39 -0700

    bg-image

--- mblog/settings.py
+++ mblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!(zry^e8!d=y6cke-c1x*s^swhtthl^mg8-rr^#3a-ddo2yjm@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
